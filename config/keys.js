b29d7a7e77996721406c0789f8fdfbb932452dad
oshry <oshrylevy@gmail.com>
2019-07-15 22:43:58 +0300

    fix config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://localhost:27017',
-    secretKey: '123456'
-};
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
+}else{
+    module.exports = require('./keys_dev');
+}
